48389c27170347a305a7f5dab6a7d6d095ea0a4f
Invalid <zappeln@gmail.com>
2020-02-21 21:23:06 -0600

    dict counter, encoding strings

--- main.cpp
+++ main.cpp
@@ -9,14 +9,12 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <cctype>
 #include <string>
-#include <sstream>
 #include <algorithm>
-
+#include <cstring>
 
 using namespace std;
+
 
 void choicesPrompt() {
     cout << "Choose from the following options: \n"
@@ -28,77 +26,96 @@ void choicesPrompt() {
          << "Your choice: ";
 }
 
-
-bool dictLookup(string str) {
-    vector<string> book;
+int dictCount() {
+    int wordCount = 0;
     ifstream dictFile("dictionary.txt");
     for (string line; getline(dictFile, line);) {
-        book.push_back(line);
+        if (line.at(line.size()-1) < 97 ) { line.replace(line.size()-1, 1,"" ); }
+        if (line.size() >= 3) {
+            wordCount++;
+        }
+    }
+    cout << wordCount << " words of size >= 3 were read in from dictionary. "<<' '<<"\n" << endl << endl;
+}
+
+bool dictLookup(string str) {
+    bool isFound = false;
+    ifstream dictFile("dictionary.txt");
+    //str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
+    str.resize(remove_if(str.begin(), str.end(),[](char x){return !isalnum(x) && !isspace(x);})-str.begin());
+    if (str.at(0) < 97 ) { str.replace(0, 1,"" ); }
+    if (str.at(str.size()-1) < 97 ) { str.replace(str.size()-1, 1,"" ); }
+    string line;
+    for (line; getline(dictFile, line);) {
+        //int foo = strcmp(str.c_str(), line);
+        if (strcmp(line.c_str(), str.c_str()) == 0 ) {isFound = true;}
+    }
+    return isFound;
+}
+
+
+bool bookLookup(string str) {
+    bool isFound = false;
+    ifstream dictFile("dictionary.txt");
+    for (string line; getline(dictFile, line);) {
+        if (line == str) {isFound = true;}
     }
 
-    cout << "book size: " << book.size() << endl;
-    cout << "it is " << (find(book.begin(), book.end(), str) != book.end()) << endl;
+    return isFound;
+}
+
+string encodeText(string* toEncodePtr, string keyword) {
+    int eChar;
+    int keyLen = 6;
+    string decoded;
+    string toEncode = *toEncodePtr;
+    for (unsigned i = 0; i < toEncode.size(); i++) {
+        if (toEncode[i] > 96 && toEncode[i] < 123) {
+            eChar = 97 + ((toEncode[i] + keyword[i % keyLen]) - 194) % 26;
+            decoded[i] = eChar;
+        } else {
+            decoded[i] = ' ';
+            toEncodePtr[i] = '-';
+        }
+    }
+    return decoded;
 }
 
 int main() {
     int menuOption = 0;
     char returnCharacter;
-    vector<string> book;
-
-    ifstream dictFile("../TheSecretAgentByJosephConrad.txt");
-
-    //for (string line; getline(dictFile, line);) {
-        //remove_if(line.begin (), line.end (), ispunct());
-
-        //remove_copy_if(text.begin(), text.end(), back_inserter(result), ptr_fun<int, int>(&std::ispunct);
-
-        //istringstream iss(line);
-        //cout << line << endl;
-       // string word;
-       // while(iss>>word){
-            // Need to remove puncuation marks
-
-            //cout << word << endl;
-           // book.push_back(word);
-       // }
-   // }
-    //cout << "book size: " << book.size() << endl;
+    string userInput;
+    string toEncode;
+    string keyword;
     char output[100];
 
-
-
+    dictCount();
 
     //choicesPrompt();
     //cin >> menuOption;
     //returnCharacter = cin.get();
-
-    //test 1 #1
-    string userSearch = "procrastinate";
-    dictLookup(userSearch);
-
-    //test 2 #1
-    // kats
-    // should return -> kats is NOT in the dictionary.
-
-    //test 3
-    //2
-    //all generalizations are false
-    //secret
-    //returns -> spn kxfitrpbrevzsgk cii xenji
-
+    menuOption = 1;
 
 
 
     switch( menuOption) {
         case 1: // Do dictionary lookup of a word and indicate whether or not it was found.
             cout << "Enter a word to be looked up in dictionary: ";
-            cin >> userSearch;
-            dictLookup(userSearch);
+            //cin >> userInput;
+            userInput  = "procrastinate";
+            //userInput = "kats";
+            cout << userInput << (dictLookup(userInput)? " IS ":" is NOT ")  << "in the dictionary." << endl;
             break;
         case 2: // Encode some text
             cout << "Enter the text to be encoded: ";
+            //cin >> toEncode;
+            toEncode = "all generalizations are false";
             cout << "Enter a keyword for Vigenere encryption: ";
+            //cin >> keyword;
+            keyword =  "secret";
             cout << "Keyword, plainText and ciphertext are:  \n";
+            for (unsigned i = 0; i < toEncode.size(); i++) {cout << keyword[i%6];}
+            cout << endl << toEncode << "\n" << encodeText(&toEncode, keyword) << endl;
             break;
         case 3: // Decode using user-entered values
             cout << "Enter the cipherText to be decoded: ";
@@ -117,3 +134,81 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+/*
+
+cout << toEncode[i] << " keyword modo: " << (toEncode[i] + keyword[i % keyLen]);
+eChar = 97 + ((toEncode[i] + keyword[i % keyLen])-194) % 26;
+cout <<' '<< eChar << ' ';
+toEncode[i] = eChar;
+cout << toEncode[i] << endl;
+
+
+
+
+
+
+
+
+
+     //test 1 #1
+    userInput = "procrastinate";
+    //dictLookup(userSearch);
+    cout << userInput << " is " << (dictLookup(userInput)? "":"not ")  << "in the dictionary." << endl;
+
+    //test 2 #1
+    userInput = "kats";
+    //dictLookup(userSearch);
+    cout << userInput << " is " << (dictLookup(userInput)? "":"NOT ")  << "in the dictionary." << endl;
+
+    // should return -> kats is NOT in the dictionary.
+
+    //test 3
+    //2
+    string toEncode = "all generalizations are false";
+    string keyword =  "secret";
+    string encoded = encodeText(toEncode, keyword);
+    for (int i = 0; i < toEncode.size(); i++) {cout << keyword[i%6];}
+    cout << endl;
+    cout << encoded << endl;
+    //           all generalizations are false
+    //returns -> spn kxfitrpbrevzsgk cii xenji
+
+
+
+
+
+
+
+*/
+
+// a = 97;  s = 115; s = 115;
+// l = 108; e = 101; p = 112;
+// 97+((108+101)%97
+
+
+//ifstream dictFile("../TheSecretAgentByJosephConrad.txt");
+
+//for (string line; getline(dictFile, line);) {
+//remove_if(line.begin (), line.end (), ispunct());
+
+//remove_copy_if(text.begin(), text.end(), back_inserter(result), ptr_fun<int, int>(&std::ispunct);
+
+//istringstream iss(line);
+//cout << line << endl;
+// string word;
+// while(iss>>word){
+// Need to remove puncuation marks
+
+//cout << word << endl;
+// book.push_back(word);
+// }
+// }
+//cout << "book size: " << book.size() << endl;
